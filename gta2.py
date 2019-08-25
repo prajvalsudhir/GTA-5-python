46c341d3b8faf1cfa715fd170818e48a1de391cf
prajvalsudhir <44942149+prajvalsudhir@users.noreply.github.com>
2019-08-25 23:39:26 +0530

    Add files via upload

---
+++ gta2.py
@@ -0,0 +1,5 @@
+import cv2
+
+img = cv2.imread('Grand Theft Auto V Screenshot 2019.08.15 - 22.36.06.32.png',1)
+
+print(img)
---
+++ img.py
@@ -0,0 +1,7 @@
+import cv2
+
+img = cv2.imread('itachi.jpg',1)
+
+print(img)
+
+cv2.imshow('image',img)
